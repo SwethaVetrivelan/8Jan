df9882419102acee654cb139a584a74e8fe1926d
SwethaVetrivelan <swetha5vsm@gmail.com>
2022-01-08 12:08:07 +0530

    Swetha

---
+++ src/OddNum/EvenNum.java
@@ -0,0 +1,13 @@
+package OddNum;
+
+public class EvenNum {
+	public static void main(String[] args) {
+	int n=9;
+	if(n%2==0) {
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+	}
+}
